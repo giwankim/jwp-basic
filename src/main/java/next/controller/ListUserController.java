ff1f0d12bfb863367d03c489d43a799709680d25
Giwan Kim <giwan.kim@gmail.com>
2024-03-21 20:03:41 +0900

    feat(controller): 로그인 된 사용자만이 사용자 목록 조회가 가능하다.

--- src/main/java/next/controller/ListUserController.java
+++ src/main/java/next/controller/ListUserController.java
@@ -2,13 +2,13 @@ package next.controller;
 
 import core.db.Database;
 
-import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(value = "/user/list")
 public class ListUserController extends HttpServlet {
@@ -16,6 +16,10 @@ public class ListUserController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException {
+    if (!UserSessionUtils.isLoggedIn(req.getSession())) {
+      resp.sendRedirect("/user/loginForm");
+      return;
+    }
     req.setAttribute("users", Database.findAll());
     RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
     rd.forward(req, resp);
--- src/main/java/next/controller/UserSessionUtils.java
+++ src/main/java/next/controller/UserSessionUtils.java
@@ -1,8 +1,14 @@
 package next.controller;
 
+import javax.servlet.http.HttpSession;
+
 public class UserSessionUtils {
   public static final String SESSION_USER_KEY = "user";
 
   private UserSessionUtils() {
+  }
+
+  public static boolean isLoggedIn(HttpSession session) {
+    return session.getAttribute(SESSION_USER_KEY) != null;
   }
 }
