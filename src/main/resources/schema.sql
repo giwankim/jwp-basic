f69d1613f2cff92e2eeecf804609c2e5b4eac5db
Giwan Kim <giwan.kim@gmail.com>
2024-04-02 11:30:51 +0900

    feat(sql): 질문과 답변을 위한 DB 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,28 @@ CREATE TABLE users
     email    varchar(50),
     PRIMARY KEY (user_id)
 );
+
+DROP TABLE IF EXISTS question;
+
+CREATE TABLE question
+(
+    question_id     bigint auto_increment,
+    writer          varchar(30)   NOT NULL,
+    title           varchar(50)   NOT NULL,
+    contents        varchar(5000) NOT NULL,
+    created_date    timestamp     NOT NULL,
+    count_of_answer int,
+    PRIMARY KEY (question_id)
+);
+
+DROP TABLE IF EXISTS answer;
+
+CREATE TABLE answer
+(
+    answer_id    bigint auto_increment,
+    writer       varchar(30)   NOT NULL,
+    contents     varchar(5000) NOT NULL,
+    created_date timestamp     NOT NULL,
+    question_id  bigint        NOT NULL,
+    PRIMARY KEY (answer_id)
+);
