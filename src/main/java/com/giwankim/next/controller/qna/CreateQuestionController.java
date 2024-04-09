690ce51573f7e1bc55fa38a2661136f937db1947
Giwan Kim <giwan.kim@gmail.com>
2024-04-09 14:11:14 +0900

    feat(controller): 인증된 유저만이 질문 추가가 가능하다.

--- src/main/java/com/giwankim/next/controller/qna/CreateQuestionController.java
+++ src/main/java/com/giwankim/next/controller/qna/CreateQuestionController.java
@@ -2,8 +2,10 @@ package com.giwankim.next.controller.qna;
 
 import com.giwankim.core.mvc.AbstractController;
 import com.giwankim.core.mvc.ModelAndView;
+import com.giwankim.next.controller.UserSessionUtils;
 import com.giwankim.next.dao.QuestionDao;
 import com.giwankim.next.model.Question;
+import com.giwankim.next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,8 +25,12 @@ public class CreateQuestionController extends AbstractController {
 
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    if (!UserSessionUtils.isLoggedIn(request.getSession())) {
+      return jspView("redirect:/user/loginForm");
+    }
+    User user = UserSessionUtils.getUserFromSession(request.getSession());
     Question question = new Question(
-      request.getParameter("writer"),
+      user.getUserId(),
       request.getParameter("title"),
       request.getParameter("contents"));
     logger.debug("question : {}", question);
