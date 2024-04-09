6e2ac7768eb13328c24caefead4e5074a21660d4
Giwan Kim <giwan.kim@gmail.com>
2024-04-09 14:05:49 +0900

    feat(controller): 인증된 유저만이 질문 추가 form에 접근 가능하다.

--- src/main/java/com/giwankim/next/controller/qna/CreateQuestionFormController.java
+++ src/main/java/com/giwankim/next/controller/qna/CreateQuestionFormController.java
@@ -2,6 +2,7 @@ package com.giwankim.next.controller.qna;
 
 import com.giwankim.core.mvc.AbstractController;
 import com.giwankim.core.mvc.ModelAndView;
+import com.giwankim.next.controller.UserSessionUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,9 @@ import java.io.IOException;
 public class CreateQuestionFormController extends AbstractController {
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    if (!UserSessionUtils.isLoggedIn(request.getSession())) {
+      return jspView("redirect:/user/loginForm");
+    }
     return jspView("/qna/form.jsp");
   }
 }
--- src/test/java/com/giwankim/next/controller/qna/CreateQuestionFormControllerTest.java
+++ src/test/java/com/giwankim/next/controller/qna/CreateQuestionFormControllerTest.java
@@ -12,9 +12,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static com.giwankim.Fixtures.aUser;
+import static com.giwankim.next.controller.UserSessionUtils.SESSION_USER_KEY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CreateQuestionFormControllerTest {
@@ -23,6 +27,9 @@ class CreateQuestionFormControllerTest {
 
   @Mock
   HttpServletResponse response;
+
+  @Mock
+  HttpSession session;
 
   CreateQuestionFormController sut;
 
@@ -34,7 +41,22 @@ class CreateQuestionFormControllerTest {
   @Test
   @DisplayName("질문하기 페이지를 서빙한다.")
   void shouldReturnQuestionForm() throws ServletException, IOException {
+    when(request.getSession()).thenReturn(session);
+    when(session.getAttribute(SESSION_USER_KEY)).thenReturn(aUser().build());
+
     ModelAndView mv = sut.handleRequest(request, response);
+
     assertThat(mv.getView()).isEqualTo(JspView.from("/qna/form.jsp"));
+  }
+
+  @Test
+  @DisplayName("로그인하지 않은 경우 로그인 페이지로 이동한다.")
+  void shouldRedirectToLoginFormWhenNotLoggedIn() throws ServletException, IOException {
+    when(request.getSession()).thenReturn(session);
+    when(session.getAttribute(SESSION_USER_KEY)).thenReturn(null);
+
+    ModelAndView mv = sut.handleRequest(request, response);
+
+    assertThat(mv.getView()).isEqualTo(JspView.from("redirect:/user/loginForm"));
   }
 }
