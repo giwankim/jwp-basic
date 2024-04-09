ec2b3643dddb375bf6c8b73ffca857d3bfbf93c0
Giwan Kim <giwan.kim@gmail.com>
2024-04-09 12:32:45 +0900

    feat(controller): 질문 추가 컨트롤러 추가한다.

--- src/main/java/com/giwankim/core/web/filter/CharacterEncodingFilter.java
+++ src/main/java/com/giwankim/core/web/filter/CharacterEncodingFilter.java
@@ -1,22 +1,16 @@
 package com.giwankim.core.web.filter;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
 
 @WebFilter(value = "/*")
 public class CharacterEncodingFilter implements Filter {
-
   private static final String DEFAULT_ENCODING = "UTF-8";
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-      throws IOException, ServletException {
+    throws IOException, ServletException {
     request.setCharacterEncoding(DEFAULT_ENCODING);
     response.setCharacterEncoding(DEFAULT_ENCODING);
     filterChain.doFilter(request, response);
